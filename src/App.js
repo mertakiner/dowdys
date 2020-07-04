5e9d4dbae6d51f23b1f3cc70f335f8781d1eef26
mert <mac@Merts-iMac.local>
2020-07-04 19:59:38 +0300

    Router bug fix 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Header from "./components/pages/Header";
 import Footer from "./components/pages/Footer";
 import Main from "./components/pages/Main";
@@ -8,14 +8,22 @@ import About from "./components/pages/About";
 function App() {
   return (
     <div className="container">
-      <Router>
+      {/* <Router> */}
         <Header />
-          <Switch>
+          {/* <Switch>
               <Route exact path="/" component={Main} />
               <Route exact path="/about" component={About} />
-          </Switch>
+          </Switch> */}
+        <Router>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+
+          <Route exact path="/"><Main /> </Route>
+          <Route exact path="/about"><About /> </Route>
+        </Router>
+
         <Footer />
-      </Router>
+      {/* </Router> */}
     </div>
   );
 }
--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -39,45 +39,6 @@ class Main extends Component {
 
                 ))}
                 
-                
-
-
-                {/* <div className="card image" style={{width: "18rem"}}>
-                    <img src={img2} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-                <div className="card image" style={{width: "18rem"}}>
-                    <img src={img3} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-                <div className="card image" style={{width: "18rem"}}>
-                    <img src={img4} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-                <div className="card image" style={{width: "18rem"}}>
-                    <img src={img1} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-                <div className="card image" style={{width: "18rem"}}>
-                    <img src={img2} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-                <div className="card image" style={{width: "18rem"}}> */}
-                    {/* <img src={img3} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div> */}
             </div>
         )
     }
